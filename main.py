024f9359968f95784135ea4686ff44dcd32648ba
kinoruu <jakubsiejak@gmail.com>
2022-08-05 03:04:07 +0200

    Script which allows to change image extension.
    What's new:
    - writing in .txt file list of all files which were unable to convert or other error occured
    
    To do:
    - passing the path as an argument when calling a script in a terminal

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import os
 from os import listdir
 from os.path import splitext
 from tqdm import tqdm
+import datetime
 
 target_directory = 'path'
 target = '.png'
-
-for file in tqdm(listdir(target_directory)):
-    filename, extension = splitext(file)
-    try:
-        if extension not in ['.png', '.gif', '.mp4', '.webm',  target]:
-            im = Image.open(target_directory + "/" + filename + extension)
-            im.save(target_directory + "/" + filename + target)
-            os.remove(target_directory + "/" + filename + extension)
-    except OSError:
-        print('Could not convert %s' % file)
+date = str(datetime.datetime.now())
+date = date.replace(":", "-")
+with open('errors in ' + str(date) + '.txt', 'a') as f:
+    f.write('errors:')
+    f.write("\n")
+    data = []
+    for file in tqdm(listdir(target_directory)):
+        filename, extension = splitext(file)
+        try:
+            if extension not in ['.png', '.gif', '.mp4', '.webm',  target]:
+                im = Image.open(target_directory + "/" + filename + extension)
+                im.save(target_directory + "/" + filename + target)
+                os.remove(target_directory + "/" + filename + extension)
+        except OSError:
+            f.write(str('Could not convert %s' % file))
+            f.write("\n")
